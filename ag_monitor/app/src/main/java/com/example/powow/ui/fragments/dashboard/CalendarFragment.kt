775ff3d1b837608ba15bc2ac9908cb1a936d0ca1
NAICOITS\shibilraj.kk <shibilraj.kk@naicoits.com>
2023-04-13 15:47:14 +0530

    * Implementation of Calendar Screen with dummy data and progress is completed.

--- ag_monitor/app/src/main/java/com/example/powow/ui/fragments/dashboard/CalendarFragment.kt
+++ ag_monitor/app/src/main/java/com/example/powow/ui/fragments/dashboard/CalendarFragment.kt
@@ -1,15 +1,18 @@
 package com.example.powow.ui.fragments.dashboard
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.powow.adapters.calendar.AdapterSun
 import com.example.powow.adapters.calendar.CalendarAdapter
 import com.example.powow.databinding.FragmentCalendarBinding
+import com.example.powow.models.calendar.ItemData
 import com.example.powow.utils.CalendarUtils
 import com.example.powow.utils.CalendarUtils.Companion.daysInWeekArray
 import java.time.LocalDate
@@ -17,12 +20,13 @@ import java.time.LocalDate
 class CalendarFragment : Fragment() {
 
     private lateinit var binding: FragmentCalendarBinding
-
+    lateinit var adapterSun: AdapterSun
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,10 +34,124 @@ class CalendarFragment : Fragment() {
         binding = FragmentCalendarBinding.inflate(inflater, container, false)
         CalendarUtils.selectedDate = LocalDate.now()
         setWeekView()
+        initRecyclerSun()
+//        initRecyclerLoader()
         initClicks()
         return binding.root
     }
 
+    private fun initRecyclerSun() {
+        val dataSun = ArrayList<ItemData>()
+        dataSun.add(ItemData("1"))
+        dataSun.add(ItemData(" "))
+        dataSun.add(ItemData(" "))
+        dataSun.add(ItemData("1"))
+        dataSun.add(ItemData("1"))
+        dataSun.add(ItemData("1"))
+        dataSun.add(ItemData("1"))
+        dataSun.add(ItemData(" "))
+        dataSun.add(ItemData( " "))
+        dataSun.add(ItemData("1"))
+        dataSun.add(ItemData("1"))
+
+        val dataMon = ArrayList<ItemData>()
+        dataMon.add(ItemData(" "))
+        dataMon.add(ItemData("1"))
+        dataMon.add(ItemData(" "))
+        dataMon.add(ItemData(" "))
+        dataMon.add(ItemData("1"))
+        dataMon.add(ItemData("1"))
+        dataMon.add(ItemData("1"))
+        dataMon.add(ItemData("1"))
+        dataMon.add(ItemData(  " "))
+        dataMon.add(ItemData(" "))
+        dataMon.add(ItemData("1"))
+
+        val dataTue = ArrayList<ItemData>()
+        dataTue.add(ItemData("1"))
+        dataTue.add(ItemData("1"))
+        dataTue.add(ItemData("1"))
+        dataTue.add(ItemData(" "))
+        dataTue.add(ItemData(" "))
+        dataTue.add(ItemData("1"))
+        dataTue.add(ItemData(" "))
+        dataTue.add(ItemData(" "))
+        dataTue.add(ItemData(  "1"))
+        dataTue.add(ItemData(" "))
+        dataTue.add(ItemData("1"))
+
+        val dataWed = ArrayList<ItemData>()
+        dataWed.add(ItemData("1"))
+        dataWed.add(ItemData(" "))
+        dataWed.add(ItemData("1"))
+        dataWed.add(ItemData(" "))
+        dataWed.add(ItemData("1"))
+        dataWed.add(ItemData(" "))
+        dataWed.add(ItemData("1"))
+        dataWed.add(ItemData(" "))
+        dataWed.add(ItemData(  "1"))
+        dataWed.add(ItemData(" "))
+        dataWed.add(ItemData("1"))
+
+        val dataThu = ArrayList<ItemData>()
+        dataThu.add(ItemData(" "))
+        dataThu.add(ItemData("1"))
+        dataThu.add(ItemData("1"))
+        dataThu.add(ItemData("1"))
+        dataThu.add(ItemData("1"))
+        dataThu.add(ItemData(" "))
+        dataThu.add(ItemData("1"))
+        dataThu.add(ItemData("1"))
+        dataThu.add(ItemData(  "1"))
+        dataThu.add(ItemData(" "))
+        dataThu.add(ItemData("1"))
+
+        val dataFri = ArrayList<ItemData>()
+        dataFri.add(ItemData("1"))
+        dataFri.add(ItemData("1"))
+        dataFri.add(ItemData("1"))
+        dataFri.add(ItemData("1"))
+        dataFri.add(ItemData(" "))
+        dataFri.add(ItemData(" "))
+        dataFri.add(ItemData(" "))
+        dataFri.add(ItemData(" "))
+        dataFri.add(ItemData(  "1"))
+        dataFri.add(ItemData("1"))
+        dataFri.add(ItemData("1"))
+
+        val dataSat = ArrayList<ItemData>()
+        dataSat.add(ItemData("1"))
+        dataSat.add(ItemData("1"))
+        dataSat.add(ItemData(" "))
+        dataSat.add(ItemData(" "))
+        dataSat.add(ItemData("1"))
+        dataSat.add(ItemData(" "))
+        dataSat.add(ItemData(" "))
+        dataSat.add(ItemData(" "))
+        dataSat.add(ItemData(  "1"))
+        dataSat.add(ItemData("1"))
+        dataSat.add(ItemData("1"))
+
+        val dataLoad = ArrayList<ItemData>()
+        dataLoad.add(ItemData("35"))
+        dataLoad.add(ItemData("35"))
+        dataLoad.add(ItemData("35"))
+        dataLoad.add(ItemData("45"))
+        dataLoad.add(ItemData("45"))
+        dataLoad.add(ItemData("45"))
+        dataLoad.add(ItemData("45"))
+        dataLoad.add(ItemData("45"))
+        dataLoad.add(ItemData( "45"))
+        dataLoad.add(ItemData("35"))
+        dataLoad.add(ItemData("35"))
+
+        adapterSun = AdapterSun(requireContext(),dataSun,dataMon,dataTue,dataWed,dataThu,dataFri,dataSat,dataLoad) { items, position ->
+
+        }
+        binding.recyclerSun.adapter = adapterSun
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun initClicks() {
         binding.arrowBack.setOnClickListener {
             CalendarUtils.selectedDate = CalendarUtils.selectedDate!!.minusWeeks(1)
